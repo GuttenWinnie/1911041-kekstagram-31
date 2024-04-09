2ee5273ebed5d0cb0a7d7795995ea879bef3a48d
GuttenWinnie <sergey.anisimov.1996@mail.ru>
2024-04-09 19:51:07 +0300

    module7-task1

--- js/main.js
+++ js/main.js
@@ -1,7 +1,4 @@
-import {getDiscript} from '/js/data.js';
 import {fragment} from './render.js';
 
 const pictureArea = document.querySelector('.pictures');
 pictureArea.appendChild(fragment);
-
-console.log(getDiscript);
